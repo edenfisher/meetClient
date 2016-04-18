158fdf55301f026f156c79c43f3f6df146007fe6
Eden Fisher <edenfisher11@gmail.comgit config --global user.email edenfisher11@gmail.comqw>
2016-04-18 08:45:59 +0300

    more progress

--- index.android.js
+++ index.android.js
@@ -8,44 +8,170 @@ import React, {
   Component,
   StyleSheet,
   Text,
-  View
+  View,
+  ListView,
+  TouchableHighlight,
+  Alert,
+  Navigator,
+  ToolbarAndroid,
+  BackAndroid
 } from 'react-native';
+import Button from 'react-native-button';
 
 class AwesomeProject extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
+      <Button
+        style={{fontSize: 10, color: 'black'}}
+        onPress={()=>console.log()}
+      >
+        Press Me!
+      </Button>
+      <Navigation />
       </View>
     );
   }
 }
 
+class Navigation extends Component{
+  render() {
+    return (
+      <Navigator
+        style={styles.container}
+        initialRoute={{id: 'first'}}
+        renderScene={this.navigatorRenderScene}/>
+    );
+  }
+
+  navigatorRenderScene(route, navigator) {
+    _navigator = navigator;
+    switch (route.id) {
+      case 'first':
+        return (<First navigator={navigator} title="first"/>);
+      case 'second':
+        return (<Second navigator={navigator} title="second" />);
+    }
+  }
+}
+var _navigator; // we fill this up upon on first navigation.
+
+BackAndroid.addEventListener('hardwareBackPress', () => {
+  if (_navigator.getCurrentRoutes().length === 1  ) {
+     return false;
+  }
+  _navigator.pop();
+  return true;
+});
+
+class Second extends React.Component{
+  render() {
+    return (
+      <View style={styles.container}>
+        <ToolbarAndroid style={styles.toolbar}
+                        title={this.props.title}
+                        onIconClicked={this.props.navigator.pop}
+                        titleColor={'#FFFFFF'}/>
+        <Text>
+          Second screen
+        </Text>
+      </View>
+    );
+  }
+};
+
+class First extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  }
+  navSecond(){
+    this.props.navigator.push({
+      id: 'second'
+    })
+  }
+  render() {
+    if (!this.state.loaded) {
+        return this.renderLoadingView();
+      }
+    return (
+      <View style={styles.container}>
+        <ToolbarAndroid style={styles.toolbar}
+                        title={this.props.title}
+                        titleColor={'#FFFFFF'}/>
+        <TouchableHighlight onPress={this.navSecond.bind(this)}>
+          <Text>Navigate to second screen</Text>
+        </TouchableHighlight>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this.renderGroup}
+          style={styles.listView}
+        />
+      </View>
+    );
+  }
+  _handlePress(event) {
+      console.log('pressed');
+  }
+  componentDidMount() {
+      this.fetchData();
+    }
+  fetchData() {
+    var groups = ["angularjs","react-native","android"];
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(groups),
+      loaded: true,
+    });
+  }
+  renderLoadingView() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading groups...
+        </Text>
+      </View>
+    );
+  }
+  _onPressButton() {
+    Alert.alert(
+          'Alert Title',
+          "alertMessage",
+        );
+  }
+  renderGroup(group) {
+    return (
+      <TouchableHighlight onPress={()=>Alert.alert(
+            'Group',
+            group,
+          )}>
+        <View style={styles.container}>
+          <Text style={styles.title}>{group}</Text>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+}
+
+
 const styles = StyleSheet.create({
   container: {
+    padding:40,
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignSelf: 'stretch',
+    flexDirection:'row',
+    backgroundColor: '#98E2FA',
+  },
+  listView: {
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    flex:1,
+    alignSelf:'center'
+  }
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
--- index.ios.js
+++ index.ios.js
@@ -8,45 +8,170 @@ import React, {
   Component,
   StyleSheet,
   Text,
-  View
+  View,
+  ListView,
+  TouchableHighlight,
+  Alert,
+  Navigator,
+  ToolbarAndroid,
+  BackAndroid
 } from 'react-native';
+import Button from 'react-native-button';
 
 class AwesomeProject extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
+    };
+  }
+  render() {
+    if (!this.state.loaded) {
+        return this.renderLoadingView();
+      }
+    return (
+      <View style={styles.container}>
+      <Button
+        style={{fontSize: 10, color: 'black'}}
+        onPress={()=>console.log()}
+      >
+        Press Me!
+      </Button>
+      <Navigation />
+      </View>
+    );
+  }
+  _handlePress(event) {
+      console.log('pressed');
+  }
+  componentDidMount() {
+      this.fetchData();
+    }
+  fetchData() {
+    var groups = ["angularjs","react-native","android"];
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(groups),
+      loaded: true,
+    });
+  }
+  renderLoadingView() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading groups...
+        </Text>
+      </View>
+    );
+  }
+  _onPressButton() {
+    Alert.alert(
+          'Alert Title',
+          "alertMessage",
+        );
+  }
+  renderGroup(group) {
+    return (
+      <TouchableHighlight onPress={()=>Alert.alert(
+            'Group',
+            group,
+          )}>
+        <View style={styles.container}>
+          <Text style={styles.title}>{group}</Text>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+}
+
+class Navigation extends Component{
+  render() {
+    return (
+      <Navigator
+        style={styles.container}
+        initialRoute={{id: 'first'}}
+        renderScene={this.navigatorRenderScene}/>
+    );
+  }
+
+  navigatorRenderScene(route, navigator) {
+    _navigator = navigator;
+    switch (route.id) {
+      case 'first':
+        return (<First navigator={navigator} title="first"/>);
+      case 'second':
+        return (<Second navigator={navigator} title="second" />);
+    }
+  }
+}
+var _navigator; // we fill this up upon on first navigation.
+
+BackAndroid.addEventListener('hardwareBackPress', () => {
+  if (_navigator.getCurrentRoutes().length === 1  ) {
+     return false;
+  }
+  _navigator.pop();
+  return true;
+});
+
+class Second extends React.Component{
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          King dorzi
+        <ToolbarAndroid style={styles.toolbar}
+                        title={this.props.title}
+                        onIconClicked={this.props.navigator.pop}
+                        titleColor={'#FFFFFF'}/>
+        <Text>
+          Second screen
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      </View>
+    );
+  }
+};
+
+class First extends React.Component{
+  navSecond(){
+    this.props.navigator.push({
+      id: 'second'
+    })
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <ToolbarAndroid style={styles.toolbar}
+                        title={this.props.title}
+                        titleColor={'#FFFFFF'}/>
+        <TouchableHighlight onPress={this.navSecond.bind(this)}>
+          <Text>Navigate to second screen</Text>
+        </TouchableHighlight>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this.renderGroup}
+          style={styles.listView}
+        />  
       </View>
     );
   }
 }
 
+
 const styles = StyleSheet.create({
   container: {
+    padding:40,
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignSelf: 'stretch',
+    flexDirection:'row',
+    backgroundColor: '#98E2FA',
+  },
+  title: {
+  },
+  listView: {
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
